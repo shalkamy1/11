69c442909a41460b33e13faa4343932fb91f2d8f
shalkamy1 <141436416+shalkamy1@users.noreply.github.com>
2023-08-29 09:05:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let data=document.querySelector('.data');
+let item=document.querySelector('.item');
+let eru=document.querySelector('.eru');
+let hate=document.querySelector('.hate');
+let showmsg=()=>{
+item.style.background='black';
+item.style.color='white';
+}
+data.addEventListener('mouseenter',showmsg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let data=document.querySelector('.data');
+// let item=document.querySelector('.item')
+// let showmsg=()=>{
+// item.style.background='black';
+// item.style.color='white';
+// }
+// data.addEventListener('mouseenter',showmsg);
